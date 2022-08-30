1c9d764ce654cb160cc2c93602cddd53e4a39803
successAgent <miesineagent@gmail.com>
2022-08-30 15:22:55 +0100

    update

--- docs/index.js
+++ docs/index.js
@@ -13,26 +13,106 @@ let tabContents = document.querySelectorAll(".works");
 let bookOne = document.querySelector(".bookOne");
 let bookTwo = document.querySelector(".bookTwo");
 let main = document.querySelector(".main");
+let basCon = document.querySelector(".basketCon");
+
+let value = 0;
 
 function showContents(books) {
   tabs.forEach((tab) =>
     tab.addEventListener("click", (e) => {
-      if (e.target.className === "tab-2-h3" || "tab-3-h3") {
-        let mainCon = document.createElement("div");
-        let div = document.createElement("div");
-        div.classList.add("div");
-        let link = document.createElement("a");
-        link.textContent = "Go back";
-        link.classList.add("link")
-        link.addEventListener("click", () => {
-          main.classList.add("show");
-          bookOne.classList.remove("show");
-          bookTwo.classList.remove("show");
-        });
-        let button = document.createElement("button");
-        button.textContent = "Add to basket";
+      let mainCon = document.createElement("div");
+      let div = document.createElement("div");
+      div.classList.add("div");
+      let link = document.createElement("a");
+      link.textContent = "Go back";
+      link.classList.add("link");
+      link.addEventListener("click", () => {
+        main.classList.add("show");
+        bookOne.classList.remove("show");
+        bookTwo.classList.remove("show");
+        basCon.classList.remove("show");
+      });
+      let button = document.createElement("button");
+      button.textContent = "Add to basket";
+      button.addEventListener("click", () => {
+        value++;
+        increateValue(value);
+      });
+      let container = document.createElement("div");
+      container.classList.add("bas-con");
+
+      if (e.target.className === "tab-4 basket-bn tabs") {
+        //if the target is tab-4, the show class will be removed from the main,bookOne and bookTwo container
+        let payBtn = document.createElement("button");
+        let parentCon = document.createElement("div");
+        parentCon.classList.add("flex")
+        let secondDiv = document.createElement("div");
+        let quantity = document.createElement("h2");
+        quantity.textContent = `X ${value}`;
+        let decreamentBtn = document.createElement("button");
+        decreamentBtn.textContent = "Remove";
+        payBtn.classList.add("pay-btn");
+        payBtn.textContent = "Pay";
+        main.classList.remove("show");
+        bookOne.classList.remove("show");
+        bookTwo.classList.remove("show");
         let container = document.createElement("div");
-        container.classList.add("bas-con");
+        //After that the show class will be added to basCon
+
+        basCon.classList.add("show");
+
+        if (value < 1) {
+          let emptyH3 = document.createElement("h3");
+          emptyH3.classList.add("empty");
+          emptyH3.textContent = "Basket is Empty";
+          mainCon.classList.add("main-con");
+          mainCon.appendChild(emptyH3);
+          container.appendChild(link);
+          container.appendChild(mainCon);
+          basCon.appendChild(container);
+          if (basCon.children.length > 1) {
+            basCon.removeChild(basCon.lastElementChild);
+          }
+        } else if (
+          (value > 0 && bookOne.children.length === 1) ||
+          (value > 0 && bookTwo.children.length === 1)
+        ) {
+          if (bookOne.children.length === 1) {
+            mainCon.classList.add("basket-con");
+            mainCon.innerHTML = `
+          <h2>${books[0].title}</h2>
+          `;
+            container.appendChild(link);
+            container.appendChild(mainCon);
+            container.appendChild(payBtn);
+            secondDiv.appendChild(quantity);
+            secondDiv.appendChild(decreamentBtn);
+            parentCon.appendChild(container);
+            parentCon.appendChild(secondDiv);
+            basCon.appendChild(parentCon);
+            if (basCon.children.length > 1) {
+              basCon.removeChild(basCon.lastElementChild);
+            }
+          }
+          if (bookTwo.children.length === 1) {
+            mainCon.classList.add("basket-con");
+            mainCon.innerHTML = `
+          <h2>${books[1].title}</h2>
+          `;
+            container.appendChild(link);
+            container.appendChild(mainCon);
+            container.appendChild(payBtn);
+            secondDiv.appendChild(quantity);
+            secondDiv.appendChild(decreamentBtn);
+            parentCon.appendChild(container);
+            parentCon.appendChild(secondDiv);
+            basCon.appendChild(parentCon);
+            if (basCon.children.length > 2) {
+              basCon.removeChild(basCon.lastElementChild);
+            }
+          }
+        }
+      } else if (e.target.className === "tab-2-h3" || "tab-3-h3") {
         container.innerHTML = `${
           e.target.className === "tab-2-h3"
             ? `<img src=${books[0].cover}/>
@@ -40,41 +120,48 @@ function showContents(books) {
             <p>${books[0].title}</p>
             <h3>Author</h3>
             <p>${books[0].author}</p>`
-            : `<img src=${books[1].cover}/>
+            : e.target.className === "tab-3-h3"
+            ? `<img src=${books[1].cover}/>
             <h3>Title</h3>
             <p>${books[1].title}</p>
             <h3>Author</h3>
             <p>${books[1].author}</p>`
+            : ""
         }`;
 
-        div.appendChild(link);
-        div.appendChild(container);
         if (e.target.className === "tab-2-h3") {
           main.classList.remove("show");
           bookOne.classList.add("show");
+          div.appendChild(link);
+          div.appendChild(container);
           mainCon.appendChild(div);
           mainCon.appendChild(button);
           bookOne.appendChild(mainCon);
           if (bookOne.children.length > 1) {
             bookOne.removeChild(bookOne.lastElementChild);
           }
-        } else {
+        } else if (e.target.className === "tab-3-h3") {
           main.classList.remove("show");
           bookTwo.classList.add("show");
+          div.appendChild(link);
+          div.appendChild(container);
           mainCon.appendChild(div);
           mainCon.appendChild(button);
           bookTwo.appendChild(mainCon);
           if (bookTwo.children.length > 1) {
             bookTwo.removeChild(bookTwo.lastElementChild);
           }
+        } else {
+          return "";
         }
       }
     })
   );
 }
 
+let empty = document.querySelector(".value");
 // function showEachBook(id) {}
 
-function removeShow() {
-  tabContents.forEach((content) => content.classList.remove("show"));
+function increateValue(value) {
+  empty.textContent = `${value < 0 ? "Empty" : value + " items"}`;
 }
